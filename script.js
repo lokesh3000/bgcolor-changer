cd580b5d4b83ad10c3af7e77d8785ec249d2d815
lokesh <bobbalalokesh801@gmail.com>
2024-08-13 18:43:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function bgchange(){
+    const val="0123456789ABCDEF";
+    let start="#";
+    for(let i=0;i<6;i++){
+        start=start+val[Math.floor(Math.random()*16)];
+    }
+    document.body.style.backgroundColor=start;
+}
+document.querySelector("body").addEventListener("keypress",bgchange);
